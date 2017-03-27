fe19a4b0870bfa4a7ee87bea0dec6afa7643f3f4
junehoe <briansung.09@gmail.com>
2017-03-27 15:30:27 -0700

    Login/registration complete

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,2 +1,15 @@
 var app = angular.module('app', ['ngRoute', 'ngMessages']);
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/loginReg.html',
+      controller: 'LoginRegController'
+    })
+    .when('/dashboard', {
+      templateUrl: 'partials/dashboard.html',
+      controller: 'DashboardController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
 })
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,2 +1,12 @@
+// require controllers
+var endorsements = require('../controllers/endorsements.js');
+var groups = require('../controllers/groups.js');
+var users = require('../controllers/users.js');
+
 module.exports = function(app) {
+  app.get('/users', users.index);
+  app.post('/login', users.login);
+  app.post('/register', users.register);
+  app.get('/logout', users.logout);
+  app.get('/current', users.current);
 }
