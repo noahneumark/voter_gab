c5282886228470721fa3c4064d360c5e55ddb9b1
zer8ne <jimothy@gmail.com>
2017-03-29 17:58:44 -0700

    prepull push

--- client/assets/controllers/groupController.js
+++ client/assets/controllers/groupController.js
@@ -1,11 +1,17 @@
-app.controller('GroupController', ['$scope', '$routeParams', '$location', 'GroupFactory', function($scope, $routeParams, $location, GroupFactory) {
+app.controller('GroupController', ['$scope', '$routeParams', '$location', 'GroupFactory', 'UserFactory',function($scope, $routeParams, $location, GroupFactory, UserFactory) {
   function show(id) {
     GroupFactory.getGroup(id, function(group) {
       $scope.group = group;
+    })
+  }
+  function getCurrentUser() {
+    UserFactory.currentUser(function(user) {
+      $scope.currentUser = user;
     })
   }
   show($routeParams.id);
   $scope.newEndorsement = function(id) {
     $location.url('/groups/'+id+'/endorsements/new')
   }
+  getCurrentUser();
 }]);
