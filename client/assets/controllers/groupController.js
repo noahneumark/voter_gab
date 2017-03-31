373e6afe4d43828410117addb1f8a1d8e3c1a2d6
junehoe <briansung.09@gmail.com>
2017-03-30 18:34:32 -0700

    Touch up on minor feature

--- client/assets/controllers/groupController.js
+++ client/assets/controllers/groupController.js
@@ -45,7 +45,7 @@ app.controller('GroupController', ['$scope', '$routeParams', '$location', 'Group
   }
   $scope.addMember = function(followerId, groupId) {
     GroupFactory.newMember(followerId, groupId, function() {
-      $location.url('/dashboard');
+      show(groupId);
     })
   }
   function getUserFollows() {
